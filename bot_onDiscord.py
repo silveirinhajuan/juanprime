f9ede2124ceca7e714f5c414682904550dba9385
silveirinhajuan <juanguerradasilveira@gmail.com>
2023-07-07 07:09:24 -0300

    ✅fix bug

--- bot_onDiscord.py
+++ bot_onDiscord.py
@@ -6,11 +6,12 @@ import asyncio
 #TODO make a wellcome to new members
 
 # This variable says whether the bot should respond to chat messages or not
-should_response = False
+
 bot_name = bot_name
 bot_nickname = 'prime'
 
 class MyClient(discord.Client):
+    should_response = False
     async def on_ready(self):
         print('Logged on as', self.user)
 
@@ -22,6 +23,7 @@ class MyClient(discord.Client):
         should_respond = False
 
     async def on_message(self, msg):
+        global should_respond
         channel = msg.channel
         if msg.author == self.user:
             return
@@ -34,8 +36,10 @@ class MyClient(discord.Client):
 
         if should_respond:
             if 'stop' in msg.content:
+                await channel.send('Beleza, depois eu volto ein😈👾')
                 should_respond = False
-            await channel.send(response_setence(msg.content))
+            else:
+                await channel.send(response_setence(msg.content))
 
 
 intents = discord.Intents.all()
